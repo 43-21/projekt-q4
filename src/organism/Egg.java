eeb2479444f972422d87589cda3d89302b2dd5dc
43-21 <68917362+43-21@users.noreply.github.com>
2024-01-25 20:34:24 +0100

    Update Egg.java

--- src/organism/Egg.java
+++ src/organism/Egg.java
@@ -1,16 +1,23 @@
 package organism;
 
 public class Egg {
+    Organism organism;
+    int timeToHatch;
+
     int time = 0;
     boolean fertilised = false;
-    Organism organism;
 
-    public Egg(Organism o) {
+    public Egg(Organism o, int t) {
         organism = o;
+        timeToHatch = t;
     }
 
-    public void isFertilised() {
+    public void setFertilised() {
         fertilised = true;
+    }
+
+    public boolean canHatch() {
+        return time > timeToHatch;
     }
 
     public void update() {
