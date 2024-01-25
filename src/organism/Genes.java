ce742b3fba60f5a5149bc0e046f5743da07fff6f
43-21 <68917362+43-21@users.noreply.github.com>
2024-01-25 20:03:17 +0100

    Update Genes.java
    speichert jetzt die gene fürs gehirn

--- src/organism/Genes.java
+++ src/organism/Genes.java
@@ -1,15 +1,33 @@
 package organism;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//Gene fürs Gehirn und den Körper des Organismus
 public class Genes {
     private int colorA;
     private int colorB;
+
+    private HashMap<Integer, NeuronGene> neuronGenes;
+    private HashSet<Synapse> synapseGenes;
+
     public Genes recombine(Genes partner) {
         return new Genes();
     }
 
-    // public Brain brain() {
-    //
-    // }
+    public Brain brain() {
+        HashMap<Integer, Neuron> neurons = new HashMap<>();
+        HashSet<Synapse> synapses = synapseGenes;
+
+        for(NeuronGene gene : neuronGenes.values()) {
+            Neuron neuron = new Neuron(gene.threshold, gene.index);
+            neurons.put(neuron.index, neuron);
+        }
+
+        return new Brain(neurons, synapses);
+    }
 
     public void mutate() {
 
@@ -25,5 +43,15 @@ public class Genes {
 
     private void mutateChangeWeights() {
 
+    }
+}
+
+class NeuronGene {
+    public float threshold;
+    public int index;
+
+    public NeuronGene(float threshold, int index) {
+        this.threshold = threshold;
+        this.index = index;
     }
 }
