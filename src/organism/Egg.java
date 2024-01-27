c07c7cf3c4ad7cf3944e6e34586892c8362be653
43-21 <68917362+43-21@users.noreply.github.com>
2024-01-27 10:55:14 +0100

    Update Egg.java
    speichert jetzt die Gene statt dem Organismus; hat ne Methode fürs befruchten

--- src/organism/Egg.java
+++ src/organism/Egg.java
@@ -1,18 +1,19 @@
 package organism;
 
 public class Egg {
-    Organism organism;
+    Genes genes;
     int timeToHatch;
 
     int time = 0;
     boolean fertilised = false;
 
-    public Egg(Organism o, int t) {
-        organism = o;
+    public Egg(Genes g, int t) {
+        genes = g;
         timeToHatch = t;
     }
 
-    public void setFertilised() {
+    public void fertilise(Genes partner) {
+        genes = genes.recombine(partner);
         fertilised = true;
     }
 
