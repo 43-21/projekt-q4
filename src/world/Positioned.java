c8578a210044834724a62bd5a6ad095c02075bba
43-21 <68917362+43-21@users.noreply.github.com>
2024-02-02 11:15:25 +0100

    bessere zufällige koordinaten

--- src/world/Positioned.java
+++ src/world/Positioned.java
@@ -1,11 +1,23 @@
 package world;
 
 import java.awt.Point;
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class Positioned {
     protected Point position;
 
     public Point getPosition() {
         return position;
+    }
+
+    public void setPosition(int x, int y) {
+        position.setLocation(x, y);
+    }
+
+    public void setRandomPosition(int width, int height) {
+        int x = ThreadLocalRandom.current().nextInt(width);
+        int y = ThreadLocalRandom.current().nextInt(height);
+
+        position.setLocation(x, y);
     }
 }
