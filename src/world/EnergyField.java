fa4b27553bffad28b6692855a70b7aea31d1ab5c
Sleek <78164389+SleekPhantom@users.noreply.github.com>
2024-02-10 19:21:12 +0100

    Food & EnergyField (unfinished)

--- src/world/EnergyField.java
+++ src/world/EnergyField.java
@@ -1,5 +1,16 @@
 package world;
 
+//aka Busch
 public class EnergyField extends Positioned {
-    
+    boolean filled;
+    int cooldown;
+
+    public EnergyField(boolean startValue, int cooldown) {
+        this.filled = startValue;
+        this.cooldown = cooldown;
+    }
+
+    public void update() {  //irgendeine Form einer update methode ist nötig um Food neu spawnen zu lassen nach Ablauf des Cooldowns
+
+    }
 }
