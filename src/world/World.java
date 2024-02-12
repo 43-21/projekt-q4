0e273965d9a66b201fc5cb60ff46b83c481846b6
43-21 <68917362+43-21@users.noreply.github.com>
2024-02-12 13:57:55 +0100

    erstmal barrieren statt unendlichkeit
    weil es einfacher umzusetzen ist, müssen die organismen erstmal in den grenzen bleiben
    außerdem schlüpfen eier jetzt und im konstruktor gibt es organismus statt ei (für tests).

--- src/world/World.java
+++ src/world/World.java
@@ -1,6 +1,5 @@
 package world;
 
-import java.awt.Point;
 import java.util.ArrayList;
 
 import organism.Egg;
@@ -21,7 +20,11 @@ public class World {
         egg.setRandomPosition(width, height);
 
         objects = new Matrix(16, 10, width, height);
-        objects.add(egg);
+        //objects.add(egg);
+
+        Organism organism = new Organism(new Genes());
+        organism.setRandomPosition(width, height);
+        objects.add(organism);
     }
 
     public void update() {
@@ -32,8 +35,8 @@ public class World {
 
         for(Positioned o : objects) {
             if(o instanceof Organism) {
-                ArrayList<Positioned> candidates = objects.searchRay(((Organism) o).position, ((Organism) o).getRotation(), 50f);
-                Positioned visible;
+                // ArrayList<Positioned> candidates = objects.searchRay(((Organism) o).position, ((Organism) o).getRotation(), 50f);
+                // Positioned visible;
                 boolean[] inputs = new boolean[8]; //3 für farbe, 2 für entfernung, 3 für kommunikation
                 ((Organism) o).setInputs(inputs);
             }   
@@ -46,57 +49,15 @@ public class World {
 
             if(o instanceof Egg) {
                 if(((Egg) o).canHatch()) {
-                    //vorher schlüpfen mit hatch()
+                    objects.add(((Egg) o).hatch());
                     objects.remove(o);
                 }
-                o.position.translate(1, 1);
             }
 
-            if(o.position.x >= 0 && o.position.x < width && o.position.y >= 0 && o.position.y < height) continue;
-            if(!(o instanceof Organism)) continue;
-
-            Point endOfLine = Functionality.getDestinationPoint(o.position, ((Organism) o).getRotation() + Math.PI, width + height);
-            int xA, xB, yA, yB;
-            if(((Organism) o).getRotation() < Math.PI) {
-                xA = 0;
-                xB = width - 1;
-                yA = height - 1;
-                yB = height - 1;
-            }
-
-            else {
-                xA = 0;
-                xB = width - 1;
-                yA = 0;
-                yB = 0;
-            }
-            Point a = new Point(xA, yA);
-            Point b = new Point(xB, yB);
-            Point intersectionPoint = Functionality.getIntersectionPoint(a, b, o.position, endOfLine);
-            System.out.println(intersectionPoint);
-            if(intersectionPoint != null) {
-                o.position = intersectionPoint;
-                continue;
-            }
-            
-            if(((Organism) o).getRotation() < Math.PI / 2.0 || ((Organism) o).getRotation() > Math.PI * (3.0 / 2.0)) {
-                xA = width - 1;
-                xB = width - 1;
-                yA = 0;
-                yB = height - 1;
-            }
-
-            else {
-                xA = 0;
-                xB = 0;
-                yA = 0;
-                yB = height - 1;
-            }
-            a = new Point(xA, yA);
-            b = new Point(xB, yB);
-            intersectionPoint = Functionality.getIntersectionPoint(a, b, o.position, endOfLine);
-            o.position = intersectionPoint;
-            System.out.println(intersectionPoint);
+            if(o.position.x < 0) o.position.x = 0;
+            else if(o.position.x >= width) o.position.x = width - 1;
+            if(o.position.y < 0) o.position.y = 0;
+            else if(o.position.y >= height) o.position.y = height - 1;
         }
     }
 
