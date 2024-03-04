2bfcdb919575ad6a411637f0ef135b6014e40045
H1detoschi <157819485+H1detoschi@users.noreply.github.com>
2024-03-04 19:59:18 +0100

    GUI
    mit JFrame (hässlich aber sollte funktionieren)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import graphics.Display;
-import world.World;
-import support.Options;
 
 class Main {
     public static void main(String[] args) {
-        new Thread(new Loop(new World(Options.width, Options.height), new Display(Options.width, Options.height))).start();
+        
     }
 }
