0d0453482cfb9f299f319516620514ba1385126e
43-21 <68917362+43-21@users.noreply.github.com>
2024-03-11 17:47:43 +0100

    schöner
    unnötigen code entfernt

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,8 @@
 package main;
-import graphics.Display;
 import gui.GUI;
-import support.Options;
-import world.World;
 
 class Main { //startet GUI
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        //gui.button.updateUI();
-        //new Thread(new Loop(new World(Options.width, Options.height), new Display(Options.width, Options.height))).start();
+        new GUI();
     }
 }
